5a7a2f695077ded6a526745590b6a7199dceb812
Arnav Gupta <arnav@codingblocks.com>
2019-10-20 14:46:12 +0530

    main activity flow
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

--- app/src/main/java/in/tosc/alfred/MainActivity.kt
+++ app/src/main/java/in/tosc/alfred/MainActivity.kt
@@ -3,16 +3,27 @@ package `in`.tosc.alfred
 import `in`.tosc.alfred.morningactions.MorningActionsActivity
 import `in`.tosc.alfred.onboarding.OnboardingActivity
 import android.content.Intent
+import android.graphics.drawable.ColorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.content.ContextCompat
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        supportActionBar?.setBackgroundDrawable(getDrawable(R.color.colorAccentDark))
+        window?.navigationBarColor = ContextCompat.getColor(this, R.color.colorPrimaryDark)
 
-//        startActivity(Intent(this, MorningActionsActivity::class.java))
-//        startActivity(Intent(this, OnboardingActivity::class.java))
+        buttonHelp.setOnClickListener {
+            startActivity(Intent(this, OnboardingActivity::class.java))
+        }
+
+        buttonTests.setOnClickListener {
+            startActivity(Intent(this, MorningActionsActivity::class.java))
+        }
+
     }
 }
